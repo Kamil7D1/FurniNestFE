02fd9bd7a5115487f8d918420163749d643301d9
Kamil <kamilcz2001@gmail.com>
2025-03-20 21:03:05 +0100

    feat: add Login component and install Formik & Yup
    - Added Login component for user authentication
    - Installed Formik for form handling
    - Installed Yup for validation
    - Implemented form validation with Yup

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,70 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Button } from "../components/Button";
+import { Link } from "react-router";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters long")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/[0-9]/, "Password must contain at least one digit")
+    .matches(
+      /[@$!%*?&]/,
+      "Password must contain at least one special character (@$!%*?&)",
+    )
+    .required("Password is required"),
+});
+
+export const LoginForm = () => {
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={() => {
+        console.log("submit");
+      }}
+    >
+      <Form className="flex w-full flex-col items-center justify-center gap-5">
+        <div className="flex w-full flex-col">
+          <label htmlFor="email">Email</label>
+          <Field
+            className="rounded-[2px] border border-(--color-dark-gray) p-[10px]"
+            name="email"
+            type="email"
+            placeholder="Email"
+          />
+          <ErrorMessage className="text-red-600" component="div" name="email" />
+        </div>
+
+        <div className="flex w-full flex-col">
+          <label htmlFor="password">Password</label>
+          <Field
+            className="rounded-[2px] border border-(--color-dark-gray) p-[10px]"
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <ErrorMessage
+            className="text-red-600"
+            component="div"
+            name="password"
+          />
+        </div>
+        <Button text="SIGN IN" path="user" />
+        <p className="lg:hidden">
+          Don't have an account?{" "}
+          <Link className="underline" to="/register">
+            Sign up
+          </Link>
+        </p>
+      </Form>
+    </Formik>
+  );
+};
---
+++ src/pages/Login.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button } from "../components/Button";
+import { LoginForm } from "../components/LoginForm";
+export const Login: React.FC = () => {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-(--color-peach-cream) px-10">
+      <h2 className="mb-15">Customer Login</h2>
+      <div className="flex">
+        <div className="flex flex-col items-center justify-center lg:w-1/2 lg:items-start">
+          <h3 className="hidden lg:block">Registered Customer</h3>
+          <p className="my-5">
+            If you have an account, sign in with your email address.
+          </p>
+          <LoginForm />
+        </div>
+        <div className="mx-15 hidden h-full w-[1px] bg-(--color-dark-gray) lg:block"></div>
+        <div className="hidden w-1/2 flex-col gap-5 lg:flex">
+          <h3>New Customers</h3>
+          <p>
+            Creating an account has many benefits: check out faster, keep more
+            than one address, track orders and more.
+          </p>
+          <Button text="CREATE AN ACCOUNT" path="/registration" />
+        </div>
+      </div>
+    </div>
+  );
+};
